2029941f99a5b85b5add3ea1aab35c2e248531ac
ProfeRoman <rscollo@hotmail.com>
2024-05-20 22:34:21 -0300

    Creacion de carpeta FrontEnd
    se creó la carpeta FrontEnd

---
+++ FrontEnd/src/app/componentes/logo-ap/logo-ap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-ap',
+  templateUrl: './logo-ap.component.html',
+  styleUrl: './logo-ap.component.css'
+})
+export class LogoAPComponent {
+
+}
